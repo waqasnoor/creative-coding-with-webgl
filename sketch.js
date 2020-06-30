3eb11163330914402dab1843200f64163e78ba32
Waqas Noor <waqas2c@yahoo.com>
2020-06-30 21:40:46 +0500

    Animating points plotting using set timeout and random index

--- sketch.js
+++ sketch.js
@@ -1,16 +1,19 @@
 const canvasSketch = require("canvas-sketch");
 const { lerp } = require("canvas-sketch-util/math");
+const random = require("canvas-sketch-util/random");
 
 const settings = {
   dimensions: [2048, 2048],
 };
-
+const getRandomIndex = (start, end) => {
+  return Math.round(lerp(start, end, Math.random()));
+};
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = "#F2F2F2";
     context.strokeStyle = "#CCC";
     context.stroke();
-    const count = 50;
+    const count = 100;
 
     context.fillRect(0, 0, width, height);
     const createGrid = () => {
@@ -26,15 +29,31 @@ const sketch = () => {
     };
     const margin = 50;
     const colors = ["#49A669", "#BF544B"];
+    random.setSeed(512);
 
-    const points = createGrid().filter(() => Math.random() > 0.5);
-    points.forEach(([u, v]) => {
-      const radius = 15;
+    let points = createGrid().filter(() => random.value() > 0.5);
+
+    function paintChart() {
+      if (!points.length) {
+        return;
+      }
+      const randIndex = getRandomIndex(0, points.length - 1);
+      const point = points[randIndex];
+      points = [
+        ...points.slice(0, randIndex),
+        ...points.slice(randIndex + 1, points.length),
+      ];
+      paintPoint(point);
+      setTimeout(paintChart, 5);
+    }
+    paintChart();
+
+    function paintPoint([u, v]) {
+      const radius = 5;
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
-      const color =
-        colors[Math.round(lerp(0, colors.length - 1, Math.random()))];
+      const color = colors[getRandomIndex(0, colors.length - 1)];
 
       console.log(Math.random());
       context.beginPath();
@@ -44,7 +63,10 @@ const sketch = () => {
       context.lineWidth = "10";
       context.strokeStyle = color;
       context.stroke();
-    });
+    }
+    // points.forEach((val) => {
+    //   paintPoint(val);
+    // });
 
     // const count = 10;
     // for (let x = 0; x < count; x++) {
