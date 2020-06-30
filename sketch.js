9f2fe3af6312a4f7b9175315fecefe52f77e51f2
Waqas Noor <waqas2c@yahoo.com>
2020-06-30 22:39:44 +0500

    Random art with random circles with random background with random colors

--- sketch.js
+++ sketch.js
@@ -1,19 +1,23 @@
 const canvasSketch = require("canvas-sketch");
 const { lerp } = require("canvas-sketch-util/math");
 const random = require("canvas-sketch-util/random");
+const palettes = require("nice-color-palettes");
 
 const settings = {
   dimensions: [2048, 2048],
 };
-const getRandomIndex = (start, end) => {
-  return Math.round(lerp(start, end, Math.random()));
+
+const pickRandomInRange = (start, end) => {
+  return Math.round(lerp(start, end, random.value()));
 };
 const sketch = () => {
+  const count = random.rangeFloor(2, 6);
+  const palette = random.shuffle(random.pick(palettes)).slice(0, count);
+  const [background] = random.shuffle(random.pick(palettes)).slice(0, 1);
   return ({ context, width, height }) => {
-    context.fillStyle = "#F2F2F2";
-    context.strokeStyle = "#CCC";
+    context.fillStyle = background;
     context.stroke();
-    const count = 100;
+    const count = 30;
 
     context.fillRect(0, 0, width, height);
     const createGrid = () => {
@@ -22,68 +26,47 @@ const sketch = () => {
         for (let y = 0; y < count; y++) {
           const u = count < 1 ? 0.5 : x / (count - 1);
           const v = count < 1 ? 0.5 : y / (count - 1);
-          points.push([u, v]);
+          points.push({
+            radius: Math.abs(random.gaussian() * 20),
+            position: [u, v],
+          });
         }
       }
       return points;
     };
-    const margin = 50;
-    const colors = ["#49A669", "#BF544B"];
-    random.setSeed(512);
+    const margin = 200;
 
+    // random.setSeed(512);
     let points = createGrid().filter(() => random.value() > 0.5);
 
     function paintChart() {
       if (!points.length) {
         return;
       }
-      const randIndex = getRandomIndex(0, points.length - 1);
+      const randIndex = pickRandomInRange(0, points.length - 1);
       const point = points[randIndex];
       points = [
         ...points.slice(0, randIndex),
         ...points.slice(randIndex + 1, points.length),
       ];
       paintPoint(point);
-      setTimeout(paintChart, 5);
+      setTimeout(paintChart, 0);
+      // paintChart();
     }
     paintChart();
 
-    function paintPoint([u, v]) {
-      const radius = 5;
+    function paintPoint({ radius, position }) {
+      const [u, v] = position;
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
-      const color = colors[getRandomIndex(0, colors.length - 1)];
+      const color = random.pick(palette);
 
-      console.log(Math.random());
       context.beginPath();
       context.arc(x, y, radius, 0, 2 * Math.PI, false);
       context.fillStyle = color;
       context.fill();
-      context.lineWidth = "10";
-      context.strokeStyle = color;
-      context.stroke();
     }
-    // points.forEach((val) => {
-    //   paintPoint(val);
-    // });
-
-    // const count = 10;
-    // for (let x = 0; x < count; x++) {
-    //   const startPositionX = x * 150;
-    //   for (let y = 1; y < count; y++) {
-    //     const u = x / count;
-    //     const v = y / count;
-    //     context.beginPath();
-    //     const startPositionY = y * 150;
-    //     context.arc(startPositionX, startPositionY, 50, 0, 2 * Math.PI, false);
-    //     context.fillStyle = "white";
-    //     context.fill();
-    //     context.lineWidth = "10";
-    //     context.strokeStyle = "black";
-    //     context.stroke();
-    //   }
-    // }
   };
 };
 
