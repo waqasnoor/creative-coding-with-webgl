d8612c1e7a811bce1b4758760f2b48254c51ba8e
Waqas Noor <waqas2c@yahoo.com>
2020-07-01 23:24:06 +0500

    Generating art with characters by rotating and transforming them

--- sketch.js
+++ sketch.js
@@ -11,23 +11,32 @@ const pickRandomInRange = (start, end) => {
   return Math.round(lerp(start, end, random.value()));
 };
 const sketch = () => {
-  const count = random.rangeFloor(2, 6);
+  const count = 5;
   const palette = random.shuffle(random.pick(palettes)).slice(0, count);
   const [background] = random.shuffle(random.pick(palettes)).slice(0, 1);
   return ({ context, width, height }) => {
     context.fillStyle = background;
-    context.stroke();
-    const count = 30;
+    const count = 50;
 
     context.fillRect(0, 0, width, height);
     const createGrid = () => {
       const points = [];
       for (let x = 0; x < count; x++) {
         for (let y = 0; y < count; y++) {
-          const u = count < 1 ? 0.5 : x / (count - 1);
-          const v = count < 1 ? 0.5 : y / (count - 1);
+          const u = count <= 1 ? 0.5 : x / (count - 1);
+          const v = count <= 1 ? 0.5 : y / (count - 1);
+          const radius = random.noise2D(u, v);
+          const rotation = random.noise2D(u, v);
+          // const characters = "==";
+          // const character =
+          //   characters[pickRandomInRange(0, characters.length - 1)];
+          const character = "=";
+          const color = random.pick(palette);
           points.push({
-            radius: Math.abs(random.gaussian() * 20),
+            character,
+            rotation,
+            color,
+            radius: Math.abs(radius) * 0.25,
             position: [u, v],
           });
         }
@@ -36,36 +45,37 @@ const sketch = () => {
     };
     const margin = 200;
 
-    // random.setSeed(512);
     let points = createGrid().filter(() => random.value() > 0.5);
 
     function paintChart() {
       if (!points.length) {
         return;
       }
-      const randIndex = pickRandomInRange(0, points.length - 1);
-      const point = points[randIndex];
+      const index = pickRandomInRange(0, points.length - 1);
+
+      const point = points[index];
       points = [
-        ...points.slice(0, randIndex),
-        ...points.slice(randIndex + 1, points.length),
+        ...points.slice(0, index),
+        ...points.slice(index + 1, points.length),
       ];
+
       paintPoint(point);
-      setTimeout(paintChart, 0);
-      // paintChart();
+      paintChart();
     }
     paintChart();
 
-    function paintPoint({ radius, position }) {
+    function paintPoint({ radius, position, color, rotation, character }) {
       const [u, v] = position;
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
+      context.save();
+      context.translate(x, y);
+      context.rotate(rotation);
 
-      const color = random.pick(palette);
-
-      context.beginPath();
-      context.arc(x, y, radius, 0, 2 * Math.PI, false);
       context.fillStyle = color;
-      context.fill();
+      context.font = `${radius * width}px "Helvetica"`;
+      context.fillText(character, 0, 0);
+      context.restore();
     }
   };
 };
